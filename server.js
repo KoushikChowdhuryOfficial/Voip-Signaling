3f58915a59f2f73a0db321d1ff51837286488901
KoushikChowdhuryOfficial <koushikchowdhuryofficial@gmail.com>
2025-08-14 13:23:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,46 @@
+// Install: npm install express socket.io
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server);
 
-io.on("connection", socket => {
-    console.log("User connected:", socket.id);
+app.use(express.static(__dirname + "/public"));
 
-    socket.on("offer", data => socket.broadcast.emit("offer", data));
-    socket.on("answer", data => socket.broadcast.emit("answer", data));
-    socket.on("ice-candidate", data => socket.broadcast.emit("ice-candidate", data));
+let users = {}; // username → socket.id
 
-    socket.on("disconnect", () => console.log("User disconnected:", socket.id));
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("register", (username) => {
+        users[username] = socket.id;
+        socket.username = username;
+        console.log(username + " registered");
+    });
+
+    socket.on("call-user", (data) => {
+        const targetSocketId = users[data.to];
+        if (targetSocketId) {
+            io.to(targetSocketId).emit("incoming-call", { from: socket.username });
+        }
+    });
+
+    socket.on("accept-call", (data) => {
+        const targetSocketId = users[data.to];
+        if (targetSocketId) {
+            io.to(targetSocketId).emit("call-accepted", { from: socket.username });
+        }
+    });
+
+    socket.on("disconnect", () => {
+        if (socket.username) {
+            delete users[socket.username];
+        }
+    });
 });
 
-server.listen(10000, () => console.log("Server running on port 10000"));
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
